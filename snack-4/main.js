3daa3bb410cd3afff5fc551261822745b1a70751
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-13 12:40:22 +0100

    add snack-4

---
+++ snack-4/main.js
@@ -0,0 +1,13 @@
+let odd = [];
+
+for (let i = 0; i < 6; i++) {
+    const userNumber = Number(prompt("Inserisci un numero", 1));
+    console.log(userNumber);
+
+    if ((userNumber + 1) % 2 === 0) {
+        odd.push(userNumber)
+    }
+}
+
+console.log(odd);
+alert(odd + " sono numeri dispari")
