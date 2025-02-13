4f889a904968fe0fa1ccb2c4ca9ce79891356bec
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-13 12:32:57 +0100

    simplify snack 3, now w/o arrays

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,5 +1,5 @@
 
-let numList = [];
+/* let numList = [];
 
 for (let i = 0; i < 10; i++) {
     const ask_number = Number(prompt("scrivi un numero"));
@@ -13,6 +13,18 @@ let total = 0;
 
 for (let index = 0; index < numList.length; index++) {
     total += parseInt(numList[index]);
+}
+
+console.log(total);
+
+alert("La somma dei numeri inseriti è: " + total) */
+
+let total = 0;
+
+for (let i = 0; i < 10; i++) {
+    const ask_number = Number(prompt("scrivi un numero"));
+    console.log(ask_number);
+    total += ask_number;
 }
 
 console.log(total);
