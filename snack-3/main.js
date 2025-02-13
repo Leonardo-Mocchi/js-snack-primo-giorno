526b3dc32ef1272cbedb55141d0933863c26fbb2
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-13 12:24:03 +0100

    add snack-3 complete

---
+++ snack-3/main.js
@@ -0,0 +1,20 @@
+
+let numList = [];
+
+for (let i = 0; i < 10; i++) {
+    const ask_number = Number(prompt("scrivi un numero"));
+    console.log(ask_number);
+    numList.push(ask_number);
+}
+
+console.log(numList);
+
+let total = 0;
+
+for (let index = 0; index < numList.length; index++) {
+    total += parseInt(numList[index]);
+}
+
+console.log(total);
+
+alert("La somma dei numeri inseriti è: " + total)
